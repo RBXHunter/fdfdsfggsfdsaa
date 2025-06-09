76a8f46ea85c1f8344684020689b08be06eafb02
RBXHunter <95361196+RBXHunter@users.noreply.github.com>
2025-06-09 04:03:49 +0200

    Update tsb.lua

--- tsb.lua
+++ tsb.lua
@@ -23,7 +23,8 @@ local scripts = {
     "https://raw.githubusercontent.com/RBXHunter/fdfdsfggsfdsaa/refs/heads/main/1sposobka",
     "https://raw.githubusercontent.com/RBXHunter/fdfdsfggsfdsaa/refs/heads/main/2sposobka",
     "https://raw.githubusercontent.com/RBXHunter/fdfdsfggsfdsaa/refs/heads/main/Runbost",
-    "https://raw.githubusercontent.com/RBXHunter/fdfdsfggsfdsaa/refs/heads/main/anims"
+    "https://raw.githubusercontent.com/RBXHunter/fdfdsfggsfdsaa/refs/heads/main/anims",
+    "https://raw.githubusercontent.com/RBXHunter/fdfdsfggsfdsaa/refs/heads/main/Sounds"
 }
 
 -- Функция для загрузки скриптов
